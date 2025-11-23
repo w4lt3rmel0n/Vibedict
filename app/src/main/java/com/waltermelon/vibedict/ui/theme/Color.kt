6e1f5599c7aac40a4c5d9866a7b9660b0fad501f
w4lt3rmel0n <forthebravery@protonmail.com>
2025-11-23 18:11:24 +1300

    Update theme colors

--- app/src/main/java/com/waltermelon/vibedict/ui/theme/Color.kt
+++ app/src/main/java/com/waltermelon/vibedict/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.waltermelon.vibedict.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFFFFFFFF)
+val PurpleGrey80 = Color(0xFFD0D0D0)
+val Pink80 = Color(0xFFBDBDBD)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(0xFF000000)
+val PurpleGrey40 = Color(0xFF696969)
+val Pink40 = Color(0xFF8D8D8D)
