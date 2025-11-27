e19556cb1729b09c554b5d9f204f605d9d9c8df9
w4lt3rmel0n <forthebravery@protonmail.com>
2025-11-28 12:32:25 +1300

    Improved stability.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "0.2.2"
+        versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         @Suppress("UnstableApiUsage")
