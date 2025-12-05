25ca85fffcc89cbc32a2bb90e9f38b59e7c53c82
w4lt3rmel0n <forthebravery@protonmail.com>
2025-12-05 21:54:16 +1300

    Re-designed UI for main page.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "0.2.4"
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         @Suppress("UnstableApiUsage")
