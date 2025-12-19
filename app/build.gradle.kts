6dd710dd1975b85a427842c3c8c8eaac0e0398e6
w4lt3rmel0n <forthebravery@protonmail.com>
2025-12-19 21:13:27 +1300

    Bump version to 0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.waltermelon.vibedict"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.5.0"
+        versionCode = 13
+        versionName = "0.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         @Suppress("UnstableApiUsage")
